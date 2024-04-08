45386d0bf7e4d903c43a7c5b5aba5fcd441dd1e4
gaby <gabrielaugartemaco@gmail.com>
2024-04-08 14:09:37 +0200

    day 33 finished

---
+++ day33/main1.py
@@ -0,0 +1,40 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 41.387920
+MY_LONG =  2.169920
+
+#https://www.latlong.net/  -> Page that gives the latitude and longitude
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+# print(data)
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+# print(data)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+
+time_now = datetime.now()
+
+print(time_now.hour)
